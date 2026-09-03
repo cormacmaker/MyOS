37e4b37a663270dda1e08e1811a16d6259211b0f
cormacmaker <declanhhegarty@gmail.com>
2026-09-03 17:32:16 -0400

    Implement welcome message in kernel_main function
    Added a welcome message display in the kernel.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,17 @@
-// MyOS kernel
-// First version
-
 void kernel_main(void)
 {
-    // MyOS starts here!
+    volatile char *video = (volatile char*)0xB8000;
+
+    const char *message = "Welcome to MyOS!";
+
+    for (int i = 0; message[i] != '\0'; i++)
+    {
+        video[i * 2] = message[i];
+        video[i * 2 + 1] = 0x07;
+    }
+
+    while (1)
+    {
+        __asm__ volatile ("hlt");
+    }
 }
